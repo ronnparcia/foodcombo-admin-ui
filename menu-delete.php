3143d0dbe649d0860f65dbef1d34499e5b2930b7
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-21 17:00:03 +0800

    Working edit and delete buttons

---
+++ menu-delete.php
@@ -0,0 +1,40 @@
+<?php require("show-errors.php"); ?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Items</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+    <!-- Custom Stylesheets -->
+    <link rel="stylesheet" href="css/global.css">
+</head>
+
+<body>
+
+    <h1>Delete</h1>
+
+    <?php
+    $itemID = $_POST["menu-item-id"];
+    echo $itemID;
+
+    require("connect-database.php");
+    $itemsSQL = "SELECT * FROM tbl_items WHERE item_id=$itemID";
+    $itemsQuery = mysqli_query($conn, $itemsSQL);
+    $itemsResult = mysqli_fetch_assoc($itemsQuery);
+
+    echo $itemsResult["item_name"];
+
+    // echo "" + $itemsResult["item_id"] + " - " + $itemsResult["item_name"];
+    ?>
+
+</body>
+
+</html>
--- menu.php
+++ menu.php
@@ -12,59 +12,77 @@
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-    
+
     <!-- Custom Stylesheets -->
     <link rel="stylesheet" href="css/global.css">
 </head>
 
 <body>
-    <!-- Navbar -->
-    <?php require("navbar.php"); ?>
-    
-    <h1>Menu Items</h1>
-    <?php require("connect-database.php"); ?>
+    <div class="container">
+        <!-- Navbar -->
+        <?php require("navbar.php"); ?>
 
-    <!-- Table of Items -->
-    <table id="menu-table" class="table">
-        <!-- Heading -->
-        <thead>
-            <tr>
-                <th>Icon</th>
-                <th>Item Name</th>
-                <th>Category</th>
-                <th>Price</th>
-                <th>Inventory Count</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </tr>
-        </thead>
-        <!-- Item Rows -->
-        <tbody>
-            <!-- SQL Statement and Query -->
-            <?php 
-            $itemsSQL = "SELECT * FROM tbl_items";
-            $itemsQuery = mysqli_query($conn, $itemsSQL);
-            ?>
+        <!-- Page Title -->
+        <h1>Menu Items</h1>
 
-            <?php while ($itemsResult = mysqli_fetch_assoc($itemsQuery)) { ?>
+        <!-- Alert Boxes -->
+        <div class="alert alert-success">Item successfully edited.</div>
+        <div class="alert alert-success">Item successfully deleted.</div>
+
+        <!-- Connect to Database -->
+        <?php require("connect-database.php"); ?>
+
+        <!-- Table of Items -->
+        <table id="menu-table" class="table table-striped">
+            <!-- Heading -->
+            <thead>
                 <tr>
-                    <td><img src="<?php echo $itemsResult["image_url"]; ?>" alt=""></td>
-                    <td><?php echo $itemsResult["item_name"]; ?></td>
-                    <td><?php echo $itemsResult["category_name"]; ?></td>
-                    <td><?php echo $itemsResult["price"]; ?></td>
-                    <td><?php echo $itemsResult["inventory_qty"]; ?></td>
-                    <!-- Edit -->
-                    <td>
-                        
-                    </td>
-                    <!-- Delete -->
-                    <td>
-                        
-                    </td>
+                    <th>Icon</th>
+                    <th>Item Name</th>
+                    <th>Category</th>
+                    <th>Price</th>
+                    <th>Inventory Count</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
                 </tr>
-            <?php } // Closing bracket for while statement ?>
-        </tbody>
-    </table> 
+            </thead>
+            <!-- Item Rows -->
+            <tbody>
+                <!-- SQL Statement and Query -->
+                <?php
+                $itemsSQL = "SELECT * FROM tbl_items ORDER BY category_name";
+                $itemsQuery = mysqli_query($conn, $itemsSQL);
+                ?>
+
+                <!-- Generate row for each record -->
+                <?php while ($itemsResult = mysqli_fetch_assoc($itemsQuery)) { ?>
+                    <tr>
+                        <td><img src="<?php echo $itemsResult["image_url"]; ?>" /></td>
+                        <td><?php echo $itemsResult["item_name"]; ?></td>
+                        <td><?php echo $itemsResult["category_name"]; ?></td>
+                        <td><?php echo $itemsResult["price"]; ?></td>
+                        <td><?php echo $itemsResult["inventory_qty"]; ?></td>
+                        <!-- Edit -->
+                        <td>
+                            <form action="menu-edit.php" method="post">
+                                <input type="hidden" name="menu-item-id" value="<?php echo $itemsResult["item_id"]; ?>" />
+                                <input type="submit" name="menu-item-edit-btn" value="Edit" />
+                            </form>
+                        </td>
+                        <!-- Delete -->
+                        <td>
+                            <form action="menu-delete.php" method="post">
+                                <input type="hidden" name="menu-item-id" value="<?php echo $itemsResult["item_id"]; ?>" />
+                                <input type="submit" name="menu-item-edit-btn" value="Delete" />
+                            </form>
+                        </td>
+                    </tr>
+                <?php } // Closing bracket for while statement 
+                ?>
+            </tbody>
+        </table>
+
+    </div>
 </body>
 
 </html>

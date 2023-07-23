fbfdd477dfae81aab42fd737aeba99a8abef8a46
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-23 15:08:19 +0800

    Cleaned navbar styles

--- reusable-ui/navbar.php
+++ reusable-ui/navbar.php
@@ -1,7 +1,9 @@
 <nav>
-    <ul>
-        <li><a href="menu.php">Menu Items</a></li>
-        <li><a href="#">Combos</a></li>
-        <li><a href="#">Daily Report</a></li>
-    </ul>
+    <div class="container">
+        <ul>
+            <li><a href="menu.php">Menu Items</a></li>
+            <li><a href="#">Combos</a></li>
+            <li><a href="#">Daily Report</a></li>
+        </ul>
+    </div>
 </nav>

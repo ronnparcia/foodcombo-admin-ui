837035a6a0a0d6ff1e12d0ae09c5b549149d3171
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-23 17:30:00 +0800

    Working SQL update code

--- menu-edit-execute.php
+++ menu-edit-execute.php
@@ -2,6 +2,7 @@
 // Connect to database
 require("reusable-snippets/connect-database.php");
 
+// Get POSTed values from form
 $id = $_POST["menu-item-id"];
 $name = $_POST["menu-item-name"];
 $category = $_POST["menu-item-category"];
@@ -9,8 +10,19 @@ $price = $_POST["menu-item-price"];
 $qty = $_POST["menu-item-qty"];
 $imageURL = $_POST["menu-item-img-url"];
 
-echo "ID $id; Name $name; Category $category; Price $price; Quantity: $qty; Image URL: $imageURL";
+// SQL
+$updateSQL = "UPDATE tbl_items
+              SET    item_name = '$name',
+                     category_name = '$category',
+                     price = $price,
+                     inventory_qty = $qty,
+                     image_url = '$imageURL'
+              WHERE  item_id = $id;";
 
-$updateSQL;
+if (mysqli_query($conn, $updateSQL)) {
+    header("location:menu.php?editSuccess=1");
+} else {
+    header("location:menu.php?editSuccess=0");
+}
 
 ?>

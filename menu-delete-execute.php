985c8f29d54e579a4e16246c0fa1c17c354a0ffe
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-23 23:28:26 +0800

    Delete script

---
+++ menu-delete-execute.php
@@ -0,0 +1,17 @@
+<?php 
+// Connect to database
+require("reusable-snippets/connect-database.php");
+
+// Get POSTed values from form
+$id = $_POST["menu-item-id"];
+
+// SQL
+$deleteSQL = "DELETE FROM tbl_items WHERE item_id = $id;";
+
+if (mysqli_query($conn, $deleteSQL)) {
+    header("location:menu.php?deleteSuccess=1");
+} else {
+    header("location:menu.php?deleteSuccess=0");
+}
+
+?>

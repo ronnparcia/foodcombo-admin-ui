bdc6068d1b25a5f8c864491c567be5dd5b57367c
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-24 18:28:13 +0800

    Added link to combos page

--- reusable-ui/navbar.php
+++ reusable-ui/navbar.php
@@ -2,7 +2,7 @@
     <div class="container">
         <ul>
             <li><a href="menu.php">Menu Items</a></li>
-            <li><a href="#">Combos</a></li>
+            <li><a href="combos.php">Combos</a></li>
             <li><a href="#">Daily Report</a></li>
         </ul>
     </div>

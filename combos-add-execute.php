e52804994eecb42c7d4e493bd71d5eddb2fa6e4f
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-24 19:38:39 +0800

    Code reability

--- combos-add-execute.php
+++ combos-add-execute.php
@@ -3,12 +3,13 @@
 require("reusable-snippets/connect-database.php");
 
 // Get POSTed values from form
-$comboName = $_POST["combo-name"];
+$comboName   = $_POST["combo-name"];
 $discountPct = $_POST["combo-discount-pct"] / 100;
 
-$mainID = $_POST["combos-main"];
-$sideID = $_POST["combos-side"];
+$mainID  = $_POST["combos-main"];
+$sideID  = $_POST["combos-side"];
 $drinkID = $_POST["combos-drink"];
+
 
 // Check if the combo is already in the database
 $existingComboSQL = "SELECT * FROM tbl_combos
@@ -17,11 +18,16 @@ $existingComboSQL = "SELECT * FROM tbl_combos
                      AND drink_item_id = $drinkID";
 $existingComboQuery = mysqli_query($conn, $existingComboSQL);
 
+
 if (mysqli_num_rows($existingComboQuery) > 0) {
+
+    // If combo exists, return to combos.php with error message
     $existingComboResult = mysqli_fetch_assoc(mysqli_query($conn, $existingComboSQL));
     $existingComboID = $existingComboResult["combo_id"];
     header("location:combos.php?comboAlreadyExists=$existingComboID");
+
 } else {
+
     // If combo doesn't exist yet, proceed with insertion
     $insertSQL = "INSERT INTO tbl_combos
                   (combo_name, main_item_id, side_item_id, drink_item_id, discount_pct)
@@ -33,6 +39,7 @@ if (mysqli_num_rows($existingComboQuery) > 0) {
     } else {
         header("location:combos.php?addSuccess=0");
     }
+
 }
 
 // Close connection

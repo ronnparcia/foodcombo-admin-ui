6fe1c8fedf5d85659a6ddb25e318e56c611f19fe
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-25 17:16:24 +0800

    Working generate report

--- report.php
+++ report.php
@@ -27,7 +27,11 @@ require("reusable-snippets/show-errors.php");
 
             <!-- Date -->
             <label for="report-date">Select date:</label>
-            <input type="date" name="report-date" class="form-control w-50" required>
+            <input type="date" 
+                   name="report-date" 
+                   max="<?php echo date("Y-m-d"); ?>"
+                   class="form-control w-50" 
+                   required>
 
             <br/>
 
@@ -38,12 +42,58 @@ require("reusable-snippets/show-errors.php");
         <br/><br/>
 
         <!-- Once Button is Clicked -->
-        <?php 
-        if (isset($_POST["report-submit-btn"])) {
-            echo "Button was clicked.";
-        }
-        
-        ?>
+        <?php if (isset($_POST["report-submit-btn"])): ?>
+
+            <!-- Connect to database -->
+            <?php 
+            $date = $_POST["report-date"];
+            
+            // Connect to database
+            require("reusable-snippets/connect-database.php");
+            $reportSQL = "SELECT SUM(main_qty) + SUM(side_qty) + SUM(drink_qty) AS total_dishes,
+                                 SUM(discounted_total_price) AS total_earned,
+                                 SUM(discount) AS total_discount
+                          FROM tbl_orders
+                          WHERE order_date='$date';";
+            $reportQuery = mysqli_query($conn, $reportSQL);
+            $reportResult = mysqli_fetch_assoc($reportQuery);
+
+            // If there is no records for this date, show error message
+            if (
+                $reportResult["total_dishes"] == NULL &&
+                $reportResult["total_earned"] == NULL &&
+                $reportResult["total_discount"] == NULL
+            ) {
+                $hasOrders = false;
+                echo '<div class="alert alert-danger">No records found for this date.</div>';
+            } else {
+                $hasOrders = true;
+                $totalDishes = $reportResult["total_dishes"];
+                $totalEarned = $reportResult["total_earned"];
+                $totalDiscount = $reportResult["total_discount"];
+            }
+            ?>
+
+            <!-- Display record -->
+            <?php if ($hasOrders): ?>
+                <h4>Report for <?php echo date('F d, Y', strtotime($date)); ?></h4>
+                <table class="table w-50">
+                    <tr>
+                        <td>Total dishes sold: </td>
+                        <td><?php echo $totalDishes; ?></td>
+                    </tr>
+                    <tr>
+                        <td>Total discounts given: </td>
+                        <td>Php <?php echo $totalDiscount; ?></td>
+                    </tr>
+                    <tr>
+                        <td>Total earnings: </td>
+                        <td>Php <?php echo $totalEarned; ?></td>
+                    </tr>
+                </table>
+            <?php endif; // For if (hasOrders) ?>
+
+        <?php endif; // For if (isset) ?>
         
 
     </div>

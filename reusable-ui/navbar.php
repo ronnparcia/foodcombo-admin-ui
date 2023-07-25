a83b2af883839e157c583be0931f9b3edb95caf9
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-25 16:56:43 +0800

    Link to report page

--- reusable-ui/navbar.php
+++ reusable-ui/navbar.php
@@ -3,7 +3,7 @@
         <ul>
             <li><a href="menu.php">Menu Items</a></li>
             <li><a href="combos.php">Combos</a></li>
-            <li><a href="#">Daily Report</a></li>
+            <li><a href="report.php">Daily Report</a></li>
         </ul>
     </div>
 </nav>

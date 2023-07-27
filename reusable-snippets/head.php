175d04545e1a994609c8a5968c23492fabbd42ce
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-27 23:34:06 +0800

    Moved Google Fonts to head and changed heading weight

--- reusable-snippets/head.php
+++ reusable-snippets/head.php
@@ -7,3 +7,8 @@
 
 <!-- Custom Stylesheets -->
 <link rel="stylesheet" href="css/global.css">
+
+<!-- Google Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600&display=swap" rel="stylesheet">

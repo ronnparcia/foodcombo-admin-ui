fc5db2dc57ebcac85948af42798b62f07338ca1b
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-27 21:52:11 +0800

    Added logout link

--- reusable-ui/navbar.php
+++ reusable-ui/navbar.php
@@ -4,6 +4,7 @@
             <li><a href="menu.php">Menu Items</a></li>
             <li><a href="combos.php">Combos</a></li>
             <li><a href="report.php">Daily Report</a></li>
+            <li><a href="logout.php">Logout (<?php echo $_SESSION["account_name"]; ?>)</a></li>
         </ul>
     </div>
 </nav>

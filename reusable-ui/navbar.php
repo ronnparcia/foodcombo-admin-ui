9db5256f754a55e69a852cac8deb2db153870eb9
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-27 23:33:53 +0800

    Navbar styles

--- reusable-ui/navbar.php
+++ reusable-ui/navbar.php
@@ -1,10 +1,18 @@
 <nav>
-    <div class="container">
+    <div class="container d-flex justify-content-between align-items-center">
+        <!-- Logo -->
+        <img src="assets/logo.png" id="logo" alt="">    
+
+        <!-- Links -->
         <ul>
             <li><a href="menu.php">Menu Items</a></li>
             <li><a href="combos.php">Combos</a></li>
             <li><a href="report.php">Daily Report</a></li>
-            <li><a href="logout.php">Logout (<?php echo $_SESSION["account_name"]; ?>)</a></li>
         </ul>
+        
+        <!-- Logout -->
+        <div id="logout-wrapper" class="">
+            <li><a href="logout.php">Logout (<?php echo $_SESSION["account_name"]; ?>)</a></li>
+        </div>
     </div>
 </nav>

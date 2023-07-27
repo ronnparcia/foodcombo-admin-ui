59923d51519584d0c4399edd78e043ab835faabf
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-07-27 22:02:50 +0800

    Alert boxes

--- login-form.php
+++ login-form.php
@@ -18,6 +18,26 @@ require("reusable-snippets/show-errors.php");
         <!-- Page Title -->
         <h1>Login</h1>
 
+        <!-- Alert Boxes -->
+        <?php 
+
+        if (isset($_GET["queryFailed"])) {
+            echo '<div class="alert alert-danger">Account database query failed.</div>';
+        }
+        
+        if (isset($_GET["wrongCredentials"])) {
+            echo '<div class="alert alert-danger">Incorrect username and/or password.</div>';
+        }
+
+        if (isset($_GET["notLoggedIn"])) {
+            echo '<div class="alert alert-warning">You must login to view that page.</div>';
+        }
+
+        if (isset($_GET["logoutSuccess"])) {
+            echo '<div class="alert alert-success">You have been logged out.</div>';
+        }
+        ?>
+
         <!-- Login Form -->
         <form action="login-authenticate.php" method="post" class="w-50">
             <!-- Username -->

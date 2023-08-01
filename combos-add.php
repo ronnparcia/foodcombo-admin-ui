15c5e0af707f0bb00d672169811caf7d62431bb2
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-08-01 23:43:28 +0800

    Added form to white box

--- combos-add.php
+++ combos-add.php
@@ -48,83 +48,84 @@ else:
         $drinksQuery = mysqli_query($conn, $drinksSQL);
         ?>
 
+        <div class="white-box">
+            <!-- Form -->
+            <form action="combos-add-execute.php" method="post">
 
-        <!-- Form -->
-        <form action="combos-add-execute.php" method="post">
+                <!-- Combo Name -->
+                <label for="combo-name">Combo Name</label>
+                <input type="text" 
+                    name="combo-name" 
+                    class="form-control"
+                    required />
 
-            <!-- Combo Name -->
-            <label for="combo-name">Combo Name</label>
-            <input type="text" 
-                   name="combo-name" 
-                   class="form-control"
-                   required />
+                <br />
 
-            <br />
+                <!-- Mains Dropdown -->
+                <label for="combos-main">Main</label>
+                <select name="combos-main" class="form-select">
+                    <!-- Create a dropdown option for each item -->
+                    <?php while ($mainsResult = mysqli_fetch_assoc($mainsQuery)) : ?>
 
-            <!-- Mains Dropdown -->
-            <label for="combos-main">Main</label>
-            <select name="combos-main" class="form-select">
-                <!-- Create a dropdown option for each item -->
-                <?php while ($mainsResult = mysqli_fetch_assoc($mainsQuery)) : ?>
+                        <!-- Display dropdown option -->
+                        <option value="<?php echo $mainsResult["item_id"]; ?>">
+                            <?php echo $mainsResult["item_name"]; ?>
+                        </option>
 
-                    <!-- Display dropdown option -->
-                    <option value="<?php echo $mainsResult["item_id"]; ?>">
-                        <?php echo $mainsResult["item_name"]; ?>
-                    </option>
+                    <?php endwhile; ?>
+                </select>
 
-                <?php endwhile; ?>
-            </select>
+                <br />
 
-            <br />
+                <!-- Sides Dropdown -->
+                <label for="combos-side">Side</label>
+                <select name="combos-side" class="form-select">
+                    <!-- Create a dropdown option for each item -->
+                    <?php while ($sidesResult = mysqli_fetch_assoc($sidesQuery)) : ?>
 
-            <!-- Sides Dropdown -->
-            <label for="combos-side">Side</label>
-            <select name="combos-side" class="form-select">
-                <!-- Create a dropdown option for each item -->
-                <?php while ($sidesResult = mysqli_fetch_assoc($sidesQuery)) : ?>
+                        <!-- Display dropdown option -->
+                        <option value="<?php echo $sidesResult["item_id"]; ?>">
+                            <?php echo $sidesResult["item_name"]; ?>
+                        </option>
 
-                    <!-- Display dropdown option -->
-                    <option value="<?php echo $sidesResult["item_id"]; ?>">
-                        <?php echo $sidesResult["item_name"]; ?>
-                    </option>
+                    <?php endwhile; ?>
+                </select>
 
-                <?php endwhile; ?>
-            </select>
+                <br />
 
-            <br />
+                <!-- Mains Dropdown -->
+                <label for="combos-drink">Drink</label>
+                <select name="combos-drink" class="form-select">
+                    <!-- Create a dropdown option for each item -->
+                    <?php while ($drinksResult = mysqli_fetch_assoc($drinksQuery)) : ?>
 
-            <!-- Mains Dropdown -->
-            <label for="combos-drink">Drink</label>
-            <select name="combos-drink" class="form-select">
-                <!-- Create a dropdown option for each item -->
-                <?php while ($drinksResult = mysqli_fetch_assoc($drinksQuery)) : ?>
+                        <!-- Display dropdown option -->
+                        <option value="<?php echo $drinksResult["item_id"]; ?>">
+                            <?php echo $drinksResult["item_name"]; ?>
+                        </option>
 
-                    <!-- Display dropdown option -->
-                    <option value="<?php echo $drinksResult["item_id"]; ?>">
-                        <?php echo $drinksResult["item_name"]; ?>
-                    </option>
+                    <?php endwhile; ?>
+                </select>
 
-                <?php endwhile; ?>
-            </select>
+                <br />
 
-            <br />
+                <!-- Discount -->
+                <label for="combo-discount-pct">Discount (%)</label>
+                <input type="number" 
+                    name="combo-discount-pct" 
+                    class="form-control"
+                    min="1"
+                    max="100"
+                    required />
 
-            <!-- Discount -->
-            <label for="combo-discount-pct">Discount (%)</label>
-            <input type="number" 
-                   name="combo-discount-pct" 
-                   class="form-control"
-                   min="1"
-                   max="100"
-                   required />
+                <br/>
 
-            <br/>
-
-            <!-- Submit -->
-            <input type="submit" 
-                   name="combos-add-execute-btn" 
-                   value="Submit" />
-        </form>
+                <!-- Submit -->
+                <input type="submit" 
+                    name="combos-add-execute-btn" 
+                    value="Submit" />
+            </form>
+        </div>
 
 
         <!-- Close SQL Connection -->

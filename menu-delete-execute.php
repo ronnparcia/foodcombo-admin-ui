ee2a518a0749ed2f2d38d6e2f305da90fae5ad9b
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-08-03 14:29:54 +0800

    Check if first if button clicked

--- menu-delete-execute.php
+++ menu-delete-execute.php
@@ -1,20 +1,25 @@
 <?php 
-// Connect to database
-require("reusable-snippets/connect-database.php");
-
-// Get POSTed values from form
-$id = $_POST["menu-item-id"];
-
-// SQL
-$deleteSQL = "DELETE FROM tbl_items WHERE item_id = $id;";
-
-if (mysqli_query($conn, $deleteSQL)) {
-    header("location:menu.php?deleteSuccess=1");
+// Check first
+if (!isset($_POST["menu-item-delete-btn"])) {
+    header("location:menu.php?noDataSent=1");
 } else {
-    header("location:menu.php?deleteSuccess=0");
-}
+    // Connect to database
+    require("reusable-snippets/connect-database.php");
 
-// Close connection
-mysqli_close($conn);
+    // Get POSTed values from form
+    $id = $_POST["menu-item-id"];
+
+    // SQL
+    $deleteSQL = "DELETE FROM tbl_items WHERE item_id = $id;";
+
+    if (mysqli_query($conn, $deleteSQL)) {
+        header("location:menu.php?deleteSuccess=1");
+    } else {
+        header("location:menu.php?deleteSuccess=0");
+    }
+
+    // Close connection
+    mysqli_close($conn);
+} // End of else for isset
 
 ?>
